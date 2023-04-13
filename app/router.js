74542be97e6d86ac46d4af4a43900c34489ac999
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-04-13 20:07:21 +0200

    vercel config

--- app/router.js
+++ app/router.js
@@ -1,39 +1,42 @@
-const express = require('express');
+const express = require("express");
 // import of the controllers
-const playController = require('./controllers/playController.js');
-const userController = require('./controllers/userController.js');
+const playController = require("./controllers/playController.js");
+const userController = require("./controllers/userController.js");
 const updateController = require("./controllers/updateController.js");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log(req);
-    res.send('hello');
+router.get("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "hello" });
 });
 
 //play
-router.get('/play/situation/:chapterId', playController.getSituation);
-router.get('/play/storytelling/:chapterId', playController.getStory);
-router.get('/play/question/:situationId', playController.getCharacterQuestionInChapter);
-router.get('/play/character/:situationId', playController.getCharacter);
-router.get('/play/allChapter', playController.getAllChapter);
+router.get("/play/situation/:chapterId", playController.getSituation);
+router.get("/play/storytelling/:chapterId", playController.getStory);
+router.get(
+  "/play/question/:situationId",
+  playController.getCharacterQuestionInChapter
+);
+router.get("/play/character/:situationId", playController.getCharacter);
+router.get("/play/allChapter", playController.getAllChapter);
 
 // Update
-router.post('/update', updateController.updateDataGame);
+router.post("/update", updateController.updateDataGame);
 
 //inscription
-router.post('/logon', userController.logonPlayer);
+router.post("/logon", userController.logonPlayer);
 
 //connexion
-router.post('/login', userController.loginPlayer);
+router.post("/login", userController.loginPlayer);
 
-// Verifier la connexion 
-router.post('/isLogged', userController.checkIflogged);
+// Verifier la connexion
+router.post("/isLogged", userController.checkIflogged);
 
 //deconnexion
-router.post('/logout', userController.logoutPlayer);
+router.post("/logout", userController.logoutPlayer);
 
 router.use((_, res) => {
-    res.status(404).send('Service does not exists');
+  res.status(404).send("Service does not exists");
 });
 
 module.exports = router;
