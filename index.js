2cf5c5a3a72389552f206f9ca1763d4669c31e85
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-04-14 13:34:46 +0200

    test

--- index.js
+++ index.js
@@ -74,6 +74,19 @@ app.use(userMiddleware);
 
 app.use(router);
 
+app.use(function(req, res, next) {
+      // res.header("Access-Control-Allow-Origin", "*");
+      const allowedOrigins = ['https://obeach.jtdev.fr'];
+      const origin = req.headers.origin;
+      if (allowedOrigins.includes(origin)) {
+           res.setHeader('Access-Control-Allow-Origin', origin);
+      }
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+      res.header("Access-Control-Allow-credentials", true);
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+      next();
+    });
+
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT} ...`);
 });
